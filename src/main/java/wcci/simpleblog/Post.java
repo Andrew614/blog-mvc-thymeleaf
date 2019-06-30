2895eeb5394c573504323c71bb2e170e3a3efa9e
Andrew614 <azhang213@gmail.com>
2019-06-30 03:19:08 -0400

    refactor entity mapping tests, add relationship tests, and update equals and hashCode in Post class

--- src/main/java/wcci/simpleblog/Post.java
+++ src/main/java/wcci/simpleblog/Post.java
@@ -2,13 +2,13 @@ package wcci.simpleblog;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-
 
 @Entity
 public class Post {
@@ -32,16 +32,12 @@ public class Post {
 	protected Post() {
 	}
 
-	public Post(String title, Author author, Category category, String content, BlogTag...blogTags) {
+	public Post(String title, Author author, Category category, String content, BlogTag... blogTags) {
 		this.title = title;
 		this.author = author;
 		this.category = category;
 		this.content = content;
-		this.blogTags = Arrays.asList(blogTags);
-//		this.blogTags = new ArrayList<BlogTag>();
-//		for (BlogTag blogTag : blogTags) {
-//			this.blogTags.add(blogTag);
-//		}
+		this.blogTags = new HashSet<>(Arrays.asList(blogTags));
 	}
 
 	public Long getId() {
@@ -72,12 +68,7 @@ public class Post {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((author == null) ? 0 : author.hashCode());
-		result = prime * result + ((blogTags == null) ? 0 : blogTags.hashCode());
-		result = prime * result + ((category == null) ? 0 : category.hashCode());
-		result = prime * result + ((content == null) ? 0 : content.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((title == null) ? 0 : title.hashCode());
 		return result;
 	}
 
@@ -90,35 +81,10 @@ public class Post {
 		if (getClass() != obj.getClass())
 			return false;
 		Post other = (Post) obj;
-		if (author == null) {
-			if (other.author != null)
-				return false;
-		} else if (!author.equals(other.author))
-			return false;
-		if (blogTags == null) {
-			if (other.blogTags != null)
-				return false;
-		} else if (!blogTags.equals(other.blogTags))
-			return false;
-		if (category == null) {
-			if (other.category != null)
-				return false;
-		} else if (!category.equals(other.category))
-			return false;
-		if (content == null) {
-			if (other.content != null)
-				return false;
-		} else if (!content.equals(other.content))
-			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
-			return false;
-		if (title == null) {
-			if (other.title != null)
-				return false;
-		} else if (!title.equals(other.title))
 			return false;
 		return true;
 	}
--- src/test/java/wcci/simpleblog/EntityMappingsTest.java
+++ src/test/java/wcci/simpleblog/EntityMappingsTest.java
@@ -1,8 +1,11 @@
 package wcci.simpleblog;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,49 +32,105 @@ public class EntityMappingsTest {
 	@Autowired
 	private BlogTagRepository blogTagRepo;
 
+	private Author author;
+	private Category category;
+	private BlogTag blogTag;
+	private BlogTag blogTag2;
+	private Post post;
+	private Post post1;
+	private Post post2;
+
+	@Before
+	public void setup() {
+		author = new Author("Andrew", "A");
+		authorRepo.save(author);
+		category = new Category("category");
+		categoryRepo.save(category);
+		blogTag = new BlogTag("food");
+		blogTagRepo.save(blogTag);
+		blogTag2 = new BlogTag("music");
+		blogTagRepo.save(blogTag2);
+
+		post = new Post("title", author, category, "content", blogTag);
+		postRepo.save(post);
+		post1 = new Post("title1", author, category, "content1", blogTag);
+		postRepo.save(post1);
+		post2 = new Post("title2", author, category, "content2", blogTag2);
+		postRepo.save(post2);
+
+		entityManager.flush();
+		entityManager.clear();
+	}
+
 	@Test
 	public void shouldSaveAndLoadAuthor() {
 		Author author = new Author("Andrew", "A");
 		authorRepo.save(author);
-		entityManager.flush();
-		entityManager.clear();
 		Author foundAuthor = authorRepo.findById(author.getId()).get();
 		assertThat(foundAuthor.getFirstName(), is("Andrew"));
 	}
 
 	@Test
+	public void shouldGenerateAuthorId() {
+		long id = author.getId();
+		assertThat(id, is(greaterThan(0L)));
+	}
+
+	@Test
 	public void shouldSaveAndLoadCategory() {
-		Category category = new Category("category");
-		categoryRepo.save(category);
-		entityManager.flush();
-		entityManager.clear();
 		Category foundCategory = categoryRepo.findById(category.getId()).get();
 		assertThat(foundCategory.getName(), is("category"));
 	}
 
 	@Test
+	public void shouldGenerateCategoryId() {
+		long id = category.getId();
+		assertThat(id, is(greaterThan(0L)));
+	}
+
+	@Test
 	public void shouldSaveAndLoadBlogTags() {
-		BlogTag blogTag = new BlogTag("food");
-		blogTagRepo.save(blogTag);
-		entityManager.flush();
-		entityManager.clear();
 		BlogTag foundBlogTag = blogTagRepo.findById(blogTag.getId()).get();
 		assertThat(foundBlogTag.getName(), is("food"));
 	}
 
 	@Test
+	public void shouldGenerateBlogTagId() {
+		long id = blogTag.getId();
+		assertThat(id, is(greaterThan(0L)));
+	}
+
+	@Test
 	public void shouldSaveAndLoadPost() {
-		Author author = new Author("Andrew", "A");
-		authorRepo.save(author);
-		Category category = new Category("category");
-		categoryRepo.save(category);
-		BlogTag blogTag = new BlogTag("fruits");
-		blogTagRepo.save(blogTag);
-		Post post = new Post("title", author, category, "content", blogTag);
-		entityManager.persist(post);
-		entityManager.flush();
-		entityManager.clear();
 		Post foundPost = postRepo.findById(post.getId()).get();
 		assertThat(foundPost.getTitle(), is("title"));
 	}
+
+	@Test
+	public void shouldGeneratePostId() {
+		long id = post.getId();
+		assertThat(id, is(greaterThan(0L)));
+	}
+
+	@Test
+	public void shouldEstablishPostToAuthorRelationship() {
+		long authorId = author.getId();
+		Author foundAuthor = authorRepo.findById(authorId).get();
+		assertThat(foundAuthor.getPosts(), containsInAnyOrder(post, post1, post2));
+	}
+
+	@Test
+	public void shouldEstablishPostToCategoryRelationship() {
+		long categoryId = category.getId();
+		Category foundCategory = categoryRepo.findById(categoryId).get();
+		assertThat(foundCategory.getPosts(), containsInAnyOrder(post, post1, post2));
+	}
+
+	@Test
+	public void shouldEstablistPostToBlogTagRelationship() {
+		long blogTagId = blogTag.getId();
+		BlogTag foundBlogTag = blogTagRepo.findById(blogTagId).get();
+		assertThat(foundBlogTag.getPosts(), containsInAnyOrder(post, post1));
+	}
+
 }
